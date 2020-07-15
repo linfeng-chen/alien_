2874c70dadea1341c3a1c85429b71bd0a1563d34
chenlinfeng <1925466036@qq.com>
2020-07-15 22:14:10 +0800

    项目-外星人入侵

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Setting():
+    '''存储游戏中的设置'''
+    def __init__(self):
+        '''初始化游戏的设置'''
+        self.screen_width=800
+        self.screen_height=600
+        self.bg_color=(255,255,255)
